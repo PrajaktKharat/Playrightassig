fa24d645fe0ccfc7de5f37fe0ed9bc3b1c5dee34
Nakul Kapre <nakulkapre@gmail.com>
2026-09-19 11:58:10 +0530

    Day1 playwright

---
+++ src/test/java/Day1/Day1_PlayrightSetupBrow.java
@@ -0,0 +1,11 @@
+package Day1;
+
+import com.microsoft.playwright.CLI;
+
+public class Day1_PlayrightSetupBrow {
+
+    public static void main(String[] args) throws  Exception {
+        CLI.main(new String[]{"install"});
+    }
+
+}
